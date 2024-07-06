cb9b6e6882bdb34ca46fa589fc870756174711dc
MaaaaD1122 <164404426+MaaaaD1122@users.noreply.github.com>
2024-07-06 23:16:29 +0300

    Update Homework32 module9.1.py

--- Homework32 module9.1.py
+++ Homework32 module9.1.py
@@ -5,31 +5,8 @@ def apply_all_func(int_list, *functions):
         results[func_name] = function(int_list)
     return results
 
-
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-
-def min_value(num):
-    return min(num)
-
-
-def max_value(num):
-    return max(num)
-
-
-def length(num):
-    return len(num)
-
-
-def total(num):
-    return sum(num)
-
-
-def sorted_list(num):
-    return sorted(num)
-
-
-result = apply_all_func(numbers, min_value, max_value, length, total, sorted_list)
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
